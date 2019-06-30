a9847be899cc1f62bd81d4f4ce4eabb738753154
erikbonn <ebonn89@gmail.com>
2019-06-29 19:42:46 -0600

    Added functional component

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import Welcome from './WelcomeMessage'
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Welcome/>
   );
 }
 
