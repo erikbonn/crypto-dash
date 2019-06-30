404ee7f94feb4e898e4d2b52770808da5b6e2dfe
erikbonn <ebonn89@gmail.com>
2019-06-29 20:22:28 -0600

    Added styled compontents

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Welcome from './WelcomeMessage'
+import WelcomeMessage from './WelcomeMessage'
+// import styled, { css } from 'styled-components';
 
-function App() {
-  return (
-    <Welcome/>
-  );
+// const MyButton = styled.div`
+//   color: green;
+//   ${props => props.primary && css`
+//     color: palevioletred;
+//   `}
+// `;
+
+// const TomatoButton = styled(MyButton)`
+//   color: tomato;
+//   border-color: tomato;
+// `;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <WelcomeMessage/>
+      </div>
+    );
+  }
 }
 
 export default App;
