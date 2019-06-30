a5caae6f54f70c9dc3dd88136acbf6539f0ba280
erikbonn <ebonn89@gmail.com>
2019-06-29 20:49:46 -0600

    Added AppBar using css grid

--- src/App/index.js
+++ src/App/index.js
@@ -2,23 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage'
 import AppLayout from './AppLayout'
-// import styled, { css } from 'styled-components';
+import AppBar from './AppBar';
 
-// const MyButton = styled.div`
-//   color: green;
-//   ${props => props.primary && css`
-//     color: palevioletred;
-//   `}
-// `;
-
-// const TomatoButton = styled(MyButton)`
-//   color: tomato;
-//   border-color: tomato;
-// `;
 class App extends Component {
   render() {
     return (
       <AppLayout>
+        <AppBar/>
         <WelcomeMessage/>
       </AppLayout>
     );
